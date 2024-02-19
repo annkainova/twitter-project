ec105cde280238711f2d5e2e26fd18eed0eadb31
Anna <cainowa@gmail.com>
2024-02-19 20:45:14 +0100

    feat: add linter setting

---
+++ public/assets/js/post_size.js
@@ -0,0 +1,35 @@
+function postSize(text) {
+  const domens = [
+    '.com',
+    '.net',
+    '.org',
+    '.ru',
+    '.de',
+    '.uk',
+    '.cn',
+    '.info',
+    '.nl',
+    '.br',
+  ];
+
+  let thisIsLink;
+  let countCharcters = 0;
+  const words = text.split(/(\s+)/);
+
+  words.forEach((word) => {
+    thisIsLink = false;
+    domens.forEach((domen) => {
+      if (word.endsWith(domen)) {
+        thisIsLink = true;
+      }
+    });
+
+    if (!thisIsLink) {
+      countCharcters += word.length;
+    }
+  });
+
+  return countCharcters;
+}
+
+export default postSize();
